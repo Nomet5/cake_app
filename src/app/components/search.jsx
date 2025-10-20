3cd02824c577b48c9748350038672fbc0e178e14
Igrees <arikzuev66@gmail.com>
2025-10-20 20:07:44 +0300

    папка с компонентами

---
+++ src/app/components/search.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+
+export default function Search() {
+
+
+    return(
+        <>
+            <div className="bg-slate-900 rounded-lg p-3">
+                
+
+                <input 
+                    type="text" 
+                    placeholder="Укажите адрес"
+                    className=" placeholder-gray-300 w-full"
+                />
+            </div>
+        </>
+
+    )
+}
